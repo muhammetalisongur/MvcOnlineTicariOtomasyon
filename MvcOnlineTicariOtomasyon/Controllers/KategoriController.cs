b8c763bce009a75f93556ef151dfbc839b38225f
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-02-27 15:12:46 +0300

    Bolum-2 Update

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,47 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
+        {
+            var kategoriler = c.Kategoris.ToList();
+            return View(kategoriler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategoribul = c.Kategoris.Find(id);
+            c.Kategoris.Remove(kategoribul);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = c.Kategoris.Find(id);
+            return View("KategoriGetir",kategori);
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var kategori = c.Kategoris.Find(k.KategoriID);
+            kategori.KategoriAd = k.KategoriAd;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # MvcOnlineTicariOtomasyon
  Mvc Online Ticari Otomasyon
+
+Code First yaklasimina gore proje hazirlanmistir.
+
+### Ekstra Eklenecekler:
+- KategoriGetiri html helper ile degil form inputu ile yap
+- 
+
+## Bolum-1
+- Siniflar Olusturuldu
+- Database migration yapildi
+- AdminLayout Olusturuldu
+
+## Bolum-2
+- AdminLayout düzenleme
+- Kategori listeleme
+- Kategori Ekleme
+- Kategori Silme
+- Kategori Guncelleme
+
+## Bolum-3
+- Ürünler Listeleme
