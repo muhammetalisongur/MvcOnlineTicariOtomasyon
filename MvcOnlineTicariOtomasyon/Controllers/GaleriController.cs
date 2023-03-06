2f401aa4e99501482e7f9f3e3cde565e1546b669
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-03-06 17:32:00 +0300

    Update

--- MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/GaleriController.cs
@@ -11,10 +11,12 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     {
         // GET: Galeri
         Context c = new Context();
+        Class1 c1 = new Class1();
         public ActionResult Index()
         {
-            var deger = c.Uruns.ToList();
-            return View(deger);
+            c1.IUruns = c.Uruns.Where(x => x.UrunID != 0).ToList();
+            c1.IDetay = c.Detays.Where(x => x.DetayID != 0).ToList();
+            return View(c1);
         }
     }
 }
