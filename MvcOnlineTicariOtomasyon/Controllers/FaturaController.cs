b05395da03e398f0fa271955cf9d6301059b7ec7
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2023-03-14 13:27:44 +0300

    Update

--- MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -79,10 +79,35 @@ namespace MvcOnlineTicariOtomasyon.Controllers
 
         public ActionResult Dinamik()
         {
-            Class3 cs = new Class3();
+            Class4 cs = new Class4();
             cs.deger1 = c.Faturalars.ToList();
             cs.deger2 = c.FaturaKalems.ToList();
             return View(cs);
+        }
+        public ActionResult FaturaKaydet(string FaturaSeriNo, string FaturaSıraNo, DateTime Tarih, string VergiDairesi, string Saat, string TeslimEden, string TeslimAlan, string Toplam, FaturaKalem[] kalemler)
+        {
+            Faturalar f = new Faturalar();
+            f.FaturaSeriNo = FaturaSeriNo;
+            f.FaturaSiraNo = FaturaSıraNo;
+            f.Tarih = Tarih;
+            f.VergiDairesi = VergiDairesi;
+            f.Saat = Saat;
+            f.TeslimEden = TeslimEden;
+            f.TeslimAlan = TeslimAlan;
+            f.Toplam = decimal.Parse(Toplam);
+            c.Faturalars.Add(f);
+            foreach (var x in kalemler)
+            {
+                FaturaKalem fk = new FaturaKalem();
+                fk.Aciklama = x.Aciklama;
+                fk.BirimFiyat = x.BirimFiyat;
+                fk.FaturaID = x.FaturaKalemID;
+                fk.Miktar = x.Miktar;
+                fk.Tutar = x.Tutar;
+                c.FaturaKalems.Add(fk);
+            }
+            c.SaveChanges();
+            return Json("İşlem Başarılı", JsonRequestBehavior.AllowGet);
         }
     }
 }
